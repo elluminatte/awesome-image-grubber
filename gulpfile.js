a17a71cccf8c2a1bfd4e2060acea414477420022
Nikolay Salov <elluminatte@gmail.com>
2016-09-04 03:49:20 +0300

    Application Gupl Task - added jade compile task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    del = require('del');
+    del = require('del'),
+    jade = require('gulp-jade-php');
 
 gulp.task('styles', function () {
     gulp.src('./assets/src/scss/**/*.scss')
@@ -8,15 +9,27 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./assets/dist/css'));
 });
 
+gulp.task('templates', function() {
+    gulp.src('./application/jade/**/*.jade')
+        .pipe(jade({
+            locals: {
+                title: 'OMG THIS IS THE TITLE'
+            }
+        }))
+        .pipe(gulp.dest('./application/views/'));
+});
+
 // Clean
 gulp.task('clean', function() {
-    return del(['assets/dist/styles', 'assets/dist/scripts', 'assets/dist/images']);
+    return del(['assets/dist/styles', 'assets/dist/scripts']);
 });
 
 gulp.task('sass:watch', ['clean'], function () {
     gulp.watch('./assets/src/scss/**/*.scss', ['styles']);
+    gulp.watch('./application/jade/**/*.jade', ['templates']);
 });
 
 gulp.task('default', function() {
     gulp.start('styles');
+    gulp.start('templates');
 });
