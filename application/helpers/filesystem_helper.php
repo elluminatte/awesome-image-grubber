375543a18bfab69df08cc9b78b2f00bfdf12f50a
Nikolay Salov <elluminatte@gmail.com>
2016-09-04 03:48:45 +0300

    Application Helpers - implemented dir name sanitize method

---
+++ application/helpers/filesystem_helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salov
+ * Date: 03.09.16
+ * Time: 0:07
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if ( ! function_exists('sanitize_directory_name'))
+{
+    function sanitize_directory_name(string $directory_name): string
+    {
+        return md5(trim(mb_strtolower($directory_name)));
+    }
+}
